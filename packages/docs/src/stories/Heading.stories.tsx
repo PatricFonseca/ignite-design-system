9b10664c7224f2d2792cd787ae2623d5de670c62
patricfonseca <patric.ccomp@gmail.com>
2023-09-09 17:47:17 -0300

    feat: add heading and avatar and auto:docs

---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,28 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Heading, HeadingProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+  args: {
+    children: 'Custom title',
+  },
+  tags: ['autodocs'],
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'H1 Heading',
+    as: 'h1',
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'By default, the heading is a `h2` element, but you can change this with the `as` prop.',
+      },
+    },
+  },
+}
